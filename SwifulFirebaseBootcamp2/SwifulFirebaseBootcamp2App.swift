df1c3e8d6b2bc30c75c97802d5382e73464cf5de
Sebastian O <sovalle2000@gmail.com>
2024-05-18 10:46:54 -0400

    Initial Commit

---
+++ SwifulFirebaseBootcamp2/SwifulFirebaseBootcamp2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwifulFirebaseBootcamp2App.swift
+//  SwifulFirebaseBootcamp2
+//
+//  Created by Macbook Air on 18-05-24.
+//
+
+import SwiftUI
+
+@main
+struct SwifulFirebaseBootcamp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
